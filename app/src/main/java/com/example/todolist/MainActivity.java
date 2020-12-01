428da7f8115978fca7219b6746571c203d74f2c5
Hisham <hishamzeyad98@gmail.com>
2020-12-02 00:59:34 +1200

    first screen

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.todolist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            this.getSupportActionBar().hide();
+
+        } catch (NullPointerException e) {
+
+        }
+
+
+
+    }
+
+
+}
