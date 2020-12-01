2a440830cb70b0bc78b1e08398dc17d58f932624
Hisham <hishamzeyad98@gmail.com>
2020-12-02 01:32:44 +1200

    second & third screen

--- app/src/main/java/com/example/todolist/Login.java
+++ app/src/main/java/com/example/todolist/Login.java
@@ -3,18 +3,16 @@ package com.example.todolist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
+    private TextView toSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_login);
     }
-
-
 }
--- app/src/main/java/com/example/todolist/SignUp.java
+++ app/src/main/java/com/example/todolist/SignUp.java
@@ -4,17 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-    }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        }
 
 
 }
