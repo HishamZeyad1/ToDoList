0ee9d17b53a5312809092bfc4767a70c50f2604c
Hisham <hishamzeyad98@gmail.com>
2020-12-26 23:53:32 +1200

    lists and tasks screen

--- app/src/main/java/com/example/todolist/TaskActivity.java
+++ app/src/main/java/com/example/todolist/TaskActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ActivityTasks extends AppCompatActivity {
+public class TaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tasks);
+        setContentView(R.layout.activity_task);
     }
 }
