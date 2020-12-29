82eb29acb0388579e580ec40b79b964f2ae393c3
Hisham <hishamzeyad98@gmail.com>
2020-12-30 11:25:34 +1200

    Apply checkbox on task screen

--- app/src/main/java/com/example/todolist/Main1Activity.java
+++ app/src/main/java/com/example/todolist/Main1Activity.java
@@ -1,23 +1,43 @@
 package com.example.todolist;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
+import com.example.todolist.R;
+import com.example.todolist.TaskAdapterEx;
+import com.example.todolist.TaskItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TaskActivity extends AppCompatActivity {
+public class Main1Activity extends AppCompatActivity {
+    TextView back,yourEmail;
+    String emailFromIntent;
     RecyclerView tasks_rv;
     TaskAdapterEx taskAdapter;
     static List<TaskItem> tasksList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task);
+        setContentView(R.layout.activity_main1);
+        back = findViewById(R.id.back);
+        yourEmail = findViewById(R.id.yourEmail);
 
+        emailFromIntent = getIntent().getStringExtra("email");
+        yourEmail.setText(emailFromIntent);
+
+        back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
 // add task items to the list
         tasksList.add(new TaskItem("Study Programming",false));
@@ -30,12 +50,5 @@ public class TaskActivity extends AppCompatActivity {
         taskAdapter = new TaskAdapterEx(this ,tasksList );
         tasks_rv.setAdapter(taskAdapter);
 
-
-
-
     }
-
-
-
-
 }
