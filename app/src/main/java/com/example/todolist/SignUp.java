36afdc102628e5cbf29b6c516b7b4f3c8e92556f
Hisham <hishamzeyad98@gmail.com>
2020-12-31 03:39:28 +1200

    Apply RecycleView on TaskScreen+Create Firebase on myApp

--- app/src/main/java/com/example/todolist/SignUp.java
+++ app/src/main/java/com/example/todolist/SignUp.java
@@ -2,28 +2,68 @@ package com.example.todolist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
 
-    TextView haveAccount;
+    TextView login,signup;
+    EditText email,password;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        haveAccount=(TextView)findViewById(R.id.haveAccount);
 
-        haveAccount.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        signup = findViewById(R.id.signup);
+
+        login=(TextView)findViewById(R.id.haveAccount);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SignUp.this, Login.class);
                 startActivity(intent);
             }
         });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignUp.this.SignUp(email.getText().toString(), password.getText().toString());
+            }
+        });
+        }
+
+
+        private  void SignUp(String email,String password){
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignUp.this, "Done Sucessfully",
+                                        Toast.LENGTH_SHORT).show();                            }
+                            // ...
+                        }
+                    });
         }
 
 
