cbbc88320f743186a2e903ac69f328bf1d64c941
Hisham <hishamzeyad98@gmail.com>
2020-12-31 23:40:08 +1200

    Doing Logout btn to back to Login Screen

--- app/src/main/java/com/example/todolist/ListActivity.java
+++ app/src/main/java/com/example/todolist/ListActivity.java
@@ -2,13 +2,31 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ListActivity extends AppCompatActivity {
-
+    TextView logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        logout=findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(ListActivity.this,"signed out", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ListActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
-}
+    }
